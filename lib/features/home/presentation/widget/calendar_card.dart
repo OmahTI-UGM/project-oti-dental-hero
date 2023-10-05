84696748299588e10f3b61435cea75603a45b10d
jon-mun <jonathan.edmund@gmail.com>
2023-10-05 18:01:30 +0700

    implement calendar data fetching

--- lib/features/home/presentation/widget/calendar_card.dart
+++ lib/features/home/presentation/widget/calendar_card.dart
@@ -1,13 +1,29 @@
 import 'package:dental_hero/core/common/color.dart';
+import 'package:dental_hero/core/constants/time_state_enum.dart';
+import 'package:dental_hero/features/activity/domain/entities/activity.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CalendarCard extends StatelessWidget {
-  CalendarCard({super.key, required this.index});
-  int index;
+  final int index;
+  final List<ActivityEntity> activityGroup;
+
+  const CalendarCard({
+    super.key,
+    required this.index,
+    required this.activityGroup,
+  });
 
   @override
   Widget build(BuildContext context) {
+    final dayActivity = activityGroup
+        .where((element) => element.timeState == TimeState.day)
+        .first;
+
+    final nightActivity = activityGroup
+        .where((element) => element.timeState == TimeState.night)
+        .first;
+
     return Container(
       width: MediaQuery.of(context).size.width * 0.167 + 20,
       height: MediaQuery.of(context).size.width * 0.167 +
@@ -18,7 +34,7 @@ class CalendarCard extends StatelessWidget {
         borderRadius: BorderRadius.circular(8),
       ),
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 8.0),
+        padding: const EdgeInsets.symmetric(horizontal: 3.0, vertical: 4.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
@@ -31,20 +47,28 @@ class CalendarCard extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Image.asset(
-                  'assets/images/night-square.png',
-                  width: MediaQuery.of(context).size.width * 0.06,
-                ),
-                Image.asset(
-                  'assets/images/day-square.png',
-                  width: MediaQuery.of(context).size.width * 0.06,
-                ),
+                _isCompleted(dayActivity, TimeState.day,
+                    MediaQuery.of(context).size.width * 0.06),
+                _isCompleted(nightActivity, TimeState.night,
+                    MediaQuery.of(context).size.width * 0.06),
               ],
             )
           ],
         ),
       ),
     );
-    ;
+  }
+
+  Widget _isCompleted(
+      ActivityEntity activity, TimeState timeState, double width) {
+    String image = timeState == TimeState.day
+        ? 'assets/images/day-square.png'
+        : 'assets/images/night-square.png';
+
+    if (activity.score != null) {
+      return Image.asset(image, width: width);
+    } else {
+      return const SizedBox.shrink();
+    }
   }
 }
